77e65cda7d167d6fc3977feb192cc8b895884cd5
unknown <e.endless.yt@gmail.com>
2026-03-09 23:03:23 +0000

    More changes to styling

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
           <input type="file" ref={fileInputRef} className="hidden" onChange={handleFile}/>
 
           <div className="py-20 text-white flex flex-col items-center gap-5">
-            <button onClick={() => fileInputRef.current.click()} className="bg-white hover:bg-slate-200 text-[#2063f5] font-bold py-2 px-4 rounded inline-flex items-center cursor-pointer">
+            <button onClick={() => fileInputRef.current.click()} className="bg-white hover:bg-slate-200 text-[#2063f5] font-bold py-2 px-4 text-xl rounded inline-flex items-center cursor-pointer">
               <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
               <span>Upload</span>
             </button>
